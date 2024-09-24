d9df0ec280245255e5652856e7e1d17daefe9755
DanielMoura17 <danielmpsouzaaa@gmail.com>
2024-09-24 09:41:16 -0300

    .

--- GCook/Services/IUsuarioService.cs
+++ GCook/Services/IUsuarioService.cs
@@ -1,12 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using GCook.ViewModels;
+using Microsoft.AspNetCore.Identity;
 
-namespace GCook.Services
+namespace GCook.Services;
+
+public interface IUsuarioService
 {
-    public interface IUsuarioService
-    {
-        
-    }
+    Task<UsuarioVM> GetUsuarioLogado();
+    Task<SignInResult> LoginUsuario(LoginVM login);
+    Task LogoffUsuario();
+    Task<List<string>> RegistrarUsuario(RegistroVM registro);
+    Task<bool> ConfirmarEmail(string userId, string code);
 }
